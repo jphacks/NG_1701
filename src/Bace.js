ed5d11dd2211195f876aa6bda2dd20509a80d1b2
chon-sakata <skt97yuma@outlook.jp>
2017-10-21 12:41:29 +0900

    友達登録で性別聞かれるよ

--- src/Bace.js
+++ src/Bace.js
@@ -5,12 +5,24 @@ var CHANNEL_ACCESS_TOKEN = 'R9CmCCDP8Bsmcvx3GIydfy1QGi43IrTbS8Y7SNvnWSTzDiiiJm+q
 // BotのuserId
 //var USER_ID = 'Uc5376a7a0a6a3ed5c8a6d6baf73220c7'
 // データを扱うスプレッドシートID
-//var SPREAD_SHEET_ID= '1e6PE_PhWesc7A98qneEKAJMILwb27Efk15HXuf3ZtwI';
+var SPREAD_SHEET_ID = '1eupv9M-SYSpra3_HNPE108fTdL1WcjmXGs2RhiA5dqA';
 /************************************************************/
 
 
 /************************************************************/
 /* 実行関数群 */
 function doPost(e) {
+    var events = JSON.parse(e.postData.contents).events;
+    events.forEach(function (event) {
+        if (event.type == "message") {
+            var init = new Init();
+            init.StartSetting(event);
+
+        } else if (event.type == "follow") {
+            var init = new Init(event);
+            init.StartSetting(event);
+        }
+    });
 }
+
 /************************************************************/
--- src/Init.js
+++ src/Init.js
@@ -1,7 +1,110 @@
 //this.<クラス内メンバ・メソッド>=(いろいろ)
 
 var Init = function () {
+    //this.event = e;
+    /*var postData = {
+        "replyToken": e.replyToken,
+        "messages": [
+            {
+                "type": "text",
+                "text": "服装提案するで～"
+            }
+            {
+                "type": "templete",
+                "altText": "性別をおしえるんやで",
+                "templete": {
+                    "type": "buttons",
+                    "title": "性別",
+                    "text": "性別なんなん",
+                    "actions": [
+                        {
+                            "type": "postback",
+                            "label": "男や",
+                            "data": "q=gender&item=male",
+                            "text": "漢やで!"
+                        },
+                        {
+                            "type": "postback",
+                            "label": "女や",
+                            "data": "q=gender&item=female",
+                            "text": "乙女やで..."
+                        },
+                        {
+                            "type": "postback",
+                            "label": "教えてたまるか",
+                            "data": "q=gender&item=secret",
+                            "text": "ひ・み・つ"
+                        }
+                    ]
+                }
+            }
+    ]
+};
 
+
+var options = {
+    "method": "post",
+    "headers": {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + CHANNEL_ACCESS_TOKEN
+    },
+    "payload": JSON.stringify(postData)
+};
+
+UrlFetchApp.fetch("https://api.line.me/v2/bot/message/reply", options); */
 };
 
 //<クラス名>.prototype.<メソッド名>  = function(引数){中身};
+Init.prototype.StartSetting = function (e) {
+    //var e = this.event;
+    var postData = {
+        "replyToken": e.replyToken,
+        "messages": [
+            /*{
+                "type": "text",
+                "text": "服装提案するで～"
+            },*/
+            {
+                "type": "template",
+                "altText": "性別をおしえるんやで",
+                "template": {
+                    "type": "buttons",
+                    "title": "性別",
+                    "text": "性別なんなん",
+                    "actions": [
+                        {
+                            "type": "message",
+                            "label": "男や",
+                            //"data": "q=gender&item=male",
+                            "text": "漢やで!"
+                        },
+                        {
+                            "type": "message",
+                            "label": "女や",
+                            //"data": "q=gender&item=female",
+                            "text": "乙女やで..."
+                        },
+                        {
+                            "type": "message",
+                            "label": "教えてたまるか",
+                            //"data": "q=gender&item=secret",
+                            "text": "ひ・み・つ"
+                        }
+                    ]
+                }
+            }
+        ]
+    };
+
+
+    var options = {
+        "method": "post",
+        "headers": {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + CHANNEL_ACCESS_TOKEN
+        },
+        "payload": JSON.stringify(postData)
+    };
+
+    UrlFetchApp.fetch("https://api.line.me/v2/bot/message/reply", options);
+};
