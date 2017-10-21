3d7fbe9998dac8ca0377a67e5d976003a8947bb1
yushi_jack <ihsuyadihcu@gmail.com>
2017-10-21 10:41:01 +0900

    botのアクセストークン

--- src/Bace.js
+++ src/Bace.js
@@ -1,3 +1,16 @@
-function doPost(e) {
+/************************************************************/
+/****** 全体で使用するキーやURLなど ******/
+// Botのアクセストークン
+var CHANNEL_ACCESS_TOKEN = 'R9CmCCDP8Bsmcvx3GIydfy1QGi43IrTbS8Y7SNvnWSTzDiiiJm+qmT+0GHk4hb4TQHuo1+2tDepoEDYtD2ExaAlTLSF024wA6iPABEFMDdmParZYsx9Wa7cXr/y81CL8cnprVdtgm6sdKEZXhbYQcwdB04t89/1O/w1cDnyilFU=';
+// BotのuserId
+//var USER_ID = 'Uc5376a7a0a6a3ed5c8a6d6baf73220c7'
+// データを扱うスプレッドシートID
+//var SPREAD_SHEET_ID= '1e6PE_PhWesc7A98qneEKAJMILwb27Efk15HXuf3ZtwI';
+/************************************************************/
 
+
+/************************************************************/
+/* 実行関数群 */
+function doPost(e) {
 }
+/************************************************************/
